6cd291e34092a3fd6335b4dd7cb110cc4a98140d
Daniel Heppner <dh@danielheppner.com>
2024-08-20 22:58:16 -0700

    build go module

--- docker.nix
+++ docker.nix
@@ -3,7 +3,7 @@
 let
   system = "x86_64-linux"; # Adjust this if you're targeting a different system
   app = pkgs.buildGoModule {
-    pname = "election-scraper";
+    pname = "go-elections";
     version = "0.1.0";
     src = ./.;
     vendorHash = "sha256-RrAecmrGKxzJAE/r/Mt+nE+9ve9H9Msz3wWeAD3w1Lk=";
@@ -12,22 +12,19 @@ let
   };
 in
 pkgs.dockerTools.buildImage {
-  name = "elections-app";
+  name = "go-elections";
   tag = "latest";
 
   copyToRoot = pkgs.buildEnv {
     name = "image-root";
     paths = [
       app
-      pkgs.gopls
-      pkgs.gotools
-      pkgs.go-tools
     ];
     pathsToLink = [ "/bin" "/etc" ];
   };
 
   config = {
-    Cmd = [ "${app}/bin/elections" ];
+    Cmd = [ "${app}/bin/go-elections" ];
     ExposedPorts = {
       "8080/tcp" = {};
     };
